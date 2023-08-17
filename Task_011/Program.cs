71d54f816fed4dbae4c141ee44f4df743b65aef8
Юлия Богатова <z.yuliya25_02@icloud.com>
2023-08-18 00:10:35 +0300

    Task 11 completed

--- Task_011/Program.cs
+++ Task_011/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу:
+// 1. которая выводит случайное трёхзначное число 
+// 2. и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число в диапазоне 100 - 999 -> {number}");
+
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+
+// int result = firstDigit * 10 + thirdDigit;
+// Console.WriteLine($"Результат после удаления второй цифры: {result}");
+
+int deleteSecondDigit = DeleteSecondDigitt(number);
+Console.WriteLine($"Результат после удаления второй цифры: {deleteSecondDigit}");
+
+int DeleteSecondDigitt(int num)
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    int result = firstDigit * 10 + thirdDigit;
+    return result;
+}
