32b527798ceac6d6405bf4ce78159286dd585bb8
Юлия Богатова <z.yuliya25_02@icloud.com>
2023-08-18 11:30:10 +0300

    Task 14 completed

--- Task_014/Program.cs
+++ Task_014/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая:
+// 1. принимает на вход число 
+// 2. и проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+bool MultiplicityTwoNumbers(int num, int num1, int num2)
+{
+    return num % num1 == 0 && num % num2 == 0;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = MultiplicityTwoNumbers(number, 7, 23);
+Console.WriteLine(result? "Кратно." : "Не кратно.");
